4966de74df57a0194cf17a87ab448de6c6ce622a
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 18:23:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,13 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
+    puts "You are too old for this."
+  end
+end
+
+def happy_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
     if age < 13
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
